6160cd6efa35a1a8b4b1adbcbcc1d8f3c06b270b
JanDa-ma <55856477+JanDa-ma@users.noreply.github.com>
2021-09-27 16:57:52 +0200

    Opdracht 2

---
+++ TestProjectV1/Calculator/CalculatorProgram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorProgram
+{
+    public class Calculator
+    {
+        public decimal Sum(params decimal[] numbers)
+        {
+            var result = 0.0M;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                result += numbers[i];
+            }
+            return result;
+        }
+    }
+}
