e0daf164e23d4a19ef4909ceff51cc65f8ef864e
JanDa-ma <55856477+JanDa-ma@users.noreply.github.com>
2021-12-20 15:10:30 +0100

    Selenium

--- WebCalculatorIT/WebCalculatorIT/Program.cs
+++ WebCalculatorIT/WebCalculatorIT/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +16,12 @@ namespace WebCalculatorIT
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            IWebDriver chromedriver = new ChromeDriver();
+            chromedriver.Navigate().GoToUrl("https://www.google.com/");
+            IWebElement inputveld = chromedriver.FindElement(By.Name("q"));
+            inputveld.SendKeys("Weer");
+            IWebElement knopzoeken = chromedriver.FindElement(By.Name("btnK"));
+            knopzoeken.Click();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
